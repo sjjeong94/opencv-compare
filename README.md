c7f62006909822f38a096aed32172abd9dc01c4a
sjjeong94 <sjjeong94@gmail.com>
2023-01-03 00:11:01 +0900

    modify package name

--- README.md
+++ README.md
@@ -1,21 +1,19 @@
-# image-compare
+# OpenCV Compare
 A simple image comparison tool, in OpenCV
 
 ## Installation
 ```bash
-git clone https://github.com/sjjeong94/image-compare
-cd image-compare
-pip install -e .
+pip install opencv-compare
 ```
 
 ## Usage
 ```
-python -m image_compare path/to/images/
+python -m opencv_compare path/to/images/
 ```
 
 ```python
 import cv2
-from image_compare import compare
+from opencv_compare import compare
 
 image1 = cv2.imread("image1.png")
 image2 = cv2.imread("image2.png")
--- setup.py
+++ setup.py
@@ -4,15 +4,15 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setup(
-    name="image-compare",
-    version="0.0.1",
+    name="opencv-compare",
+    version="0.0.2",
     author="sjjeong94",
     author_email="sjjeong94@gmail.com",
-    packages=["image_compare"],
+    packages=["opencv_compare"],
     license="MIT",
     description="A simple image comparison tool, in OpenCV",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/sjjeong94/image-compare",
+    url="https://github.com/sjjeong94/opencv-compare",
     install_requires=["opencv-python"],
 )
